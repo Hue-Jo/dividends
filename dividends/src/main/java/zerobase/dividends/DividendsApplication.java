c33f02349f11c0e167c79f247f32cfa09449423a
HueJo <slack_water@naver.com>
2024-06-28 23:35:09 +0900

    scraper scheduler

--- dividends/src/main/java/zerobase/dividends/DividendsApplication.java
+++ dividends/src/main/java/zerobase/dividends/DividendsApplication.java
@@ -2,10 +2,12 @@ package zerobase.dividends;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import zerobase.dividends.scraper.Scraper;
 import zerobase.dividends.scraper.YahooFinanceScraper;
 
 @SpringBootApplication
+@EnableScheduling
 public class DividendsApplication {
 
     public static void main(String[] args) {
---
+++ dividends/src/main/java/zerobase/dividends/Scheduler/ScraperScheduler.java
@@ -0,0 +1,70 @@
+package zerobase.dividends.Scheduler;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import zerobase.dividends.model.Company;
+import zerobase.dividends.model.ScrapedResult;
+import zerobase.dividends.persist.CompanyRepository;
+import zerobase.dividends.persist.DividendRepository;
+import zerobase.dividends.persist.entity.CompanyEntity;
+import zerobase.dividends.persist.entity.DividendEntity;
+import zerobase.dividends.scraper.Scraper;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class ScraperScheduler {
+
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+    private final Scraper yahooFinanceScraper;
+
+//    @Scheduled(fixedDelay = 1000)
+//    public void test1() throws InterruptedException {
+//        Thread.sleep(10000); // 10초간 일시정지
+//        System.out.println(Thread.currentThread().getName() + " -> test 1 " + LocalDateTime.now());
+//    }
+//    @Scheduled(fixedDelay = 1000)
+//    public void test2() {
+//        System.out.println(Thread.currentThread().getName() + " -> test 2 " + LocalDateTime.now());
+//    }
+
+    @Scheduled(cron = "${scheduler.scrap.yahoo}")
+    public void yahooFinanceScheduling() {
+        log.info("scraping scheduler is started");
+        // 저장된 회사 목록 조회
+        List<CompanyEntity> companies = this.companyRepository.findAll();
+        // 회사마다 배당금 정보 새로 스크래핑
+        for(var company : companies) {
+            ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(Company.builder()
+                    .name(company.getName())
+                    .ticker(company.getTicker())
+                    .build());
+
+            // 스크래핑한 정보 중 DB에 없는 값은 저장하기
+            scrapedResult.getDividends().stream()
+                    // dividend 모델을 엔티티로 매핑
+                    .map(e -> new DividendEntity(company.getId(), e))
+                    // 각 요소들을 하나씩 dividene 레파지토리에 삽입
+                    .forEach(e -> {
+                        boolean exists = this.dividendRepository
+                                            .existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
+                        if (!exists) {
+                            this.dividendRepository.save(e);
+                        }
+                    });
+            // 연속적으로 스크래핑 대상 사이트 서버에 요청을 날리지 않도록 일시정지 (스레드 슬립)
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+    }
+}
