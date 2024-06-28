5f7e1c9a4762e3315e83b7f26627fc86988cce44
HueJo <slack_water@naver.com>
2024-06-28 11:18:19 +0900

    financeService getDividendByCompanyName

--- dividends/src/main/java/zerobase/dividends/persist/CompanyRepository.java
+++ dividends/src/main/java/zerobase/dividends/persist/CompanyRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import zerobase.dividends.persist.entity.CompanyEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
     boolean existsByTicker(String ticker);
+
+    // Optional을 쓰는 이유 = NullPointException 방지, 값이 없는 경우에도 깔끔한 코드 구현 가능
+    Optional<CompanyEntity> findByName(String name);
+
 }
