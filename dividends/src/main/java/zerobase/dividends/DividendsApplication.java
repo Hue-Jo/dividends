cc35a079a3971927bf1020348caf2e5176a93e6d
HueJo <slack_water@naver.com>
2024-07-01 10:12:03 +0900

    cache config

--- dividends/src/main/java/zerobase/dividends/DividendsApplication.java
+++ dividends/src/main/java/zerobase/dividends/DividendsApplication.java
@@ -2,12 +2,14 @@ package zerobase.dividends;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import zerobase.dividends.scraper.Scraper;
 import zerobase.dividends.scraper.YahooFinanceScraper;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class DividendsApplication {
 
     public static void main(String[] args) {
